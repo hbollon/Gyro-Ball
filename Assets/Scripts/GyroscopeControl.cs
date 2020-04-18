88f5152dbfc7fd402bfc6a8143dd9f983e6a936d
Hugo <hugo.bollon@gmail.com>
2020-04-19 01:29:54 +0200

    GyroscopeControl for object rotation & scene setup

--- Assets/Scripts/GyroscopeControl.cs
+++ Assets/Scripts/GyroscopeControl.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class GyroscopeControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // STATE
+    private float _initialYAngle = 0f;
+    private float _appliedGyroYAngle = 0f;
+    private float _calibrationYAngle = 0f;
+    private Transform _rawGyroRotation;
+    private float _tempSmoothing;
+
+    // SETTINGS
+    [SerializeField] private float _smoothing = 0.1f;
+
+    private IEnumerator Start()
     {
-        
+        Input.gyro.enabled = true;
+        Application.targetFrameRate = 60;
+        _initialYAngle = transform.eulerAngles.y;
+
+        _rawGyroRotation = new GameObject("GyroRaw").transform;
+        _rawGyroRotation.position = transform.position;
+        _rawGyroRotation.rotation = transform.rotation;
+
+        // Wait until gyro is active, then calibrate to reset starting rotation.
+        yield return new WaitForSeconds(1);
+
+        StartCoroutine(CalibrateYAngle());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        ApplyGyroRotation();
+        ApplyCalibration();
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, _rawGyroRotation.rotation, _smoothing);
+    }
+
+    private IEnumerator CalibrateYAngle()
+    {
+        _tempSmoothing = _smoothing;
+        _smoothing = 1;
+        _calibrationYAngle = _appliedGyroYAngle - _initialYAngle; // Offsets the y angle in case it wasn't 0 at edit time.
+        yield return null;
+        _smoothing = _tempSmoothing;
+    }
+
+    private void ApplyGyroRotation()
+    {
+        _rawGyroRotation.rotation = Input.gyro.attitude;
+        _rawGyroRotation.Rotate(0f, 0f, 180f, Space.Self); // Swap "handedness" of quaternion from gyro.
+        _rawGyroRotation.Rotate(90f, 180f, 0f, Space.World); // Rotate to make sense as a camera pointing out the back of your device.
+        _appliedGyroYAngle = _rawGyroRotation.eulerAngles.y; // Save the angle around y axis for use in calibration.
+    }
+
+    private void ApplyCalibration()
+    {
+        _rawGyroRotation.Rotate(0f, -_calibrationYAngle, 0f, Space.World); // Rotates y angle back however much it deviated when calibrationYAngle was saved.
+    }
+
+    public void SetEnabled(bool value)
+    {
+        enabled = true;
+        StartCoroutine(CalibrateYAngle());
     }
 }
