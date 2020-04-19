f9aea4e4b9298cc056f024a11a6b841adcc73c53
Hugo <hugo.bollon@gmail.com>
2020-04-20 01:29:28 +0200

    Add initial offset for calibration

--- Assets/Scripts/GyroscopeControl.cs
+++ Assets/Scripts/GyroscopeControl.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class GyroscopeControl : MonoBehaviour
 {
     // STATE
-    private float _initialXAngle = 0f;
-    private float _initialYAngle = 0f;
-    private float _initialZAngle = 0f;
+    private Vector3 _initialAngles;
     private float _appliedGyroXAngle = 0f;
     private float _appliedGyroYAngle = 0f;
     private float _appliedGyroZAngle = 0f;
@@ -17,40 +15,52 @@ public class GyroscopeControl : MonoBehaviour
     private Transform _rawGyroRotation;
     private float _tempSmoothing;
 
+    Quaternion initialRotation; 
+    Quaternion gyroInitialRotation;
+
     // SETTINGS
     [SerializeField] private float _smoothing = 0.1f;
 
-    private IEnumerator Start()
-    {
+    private void Awake() {
         Input.gyro.enabled = true;
+    }
+
+    private void Start()
+    {
         Application.targetFrameRate = 60;
-        _initialYAngle = transform.eulerAngles.y;
+        
+        initialRotation = transform.rotation; 
+        gyroInitialRotation.x = Input.gyro.attitude.x;
+        gyroInitialRotation.y = 0.0f;
+        gyroInitialRotation.z = Input.gyro.attitude.y;
+        gyroInitialRotation.w = Input.gyro.attitude.w;
 
         _rawGyroRotation = new GameObject("GyroRaw").transform;
         _rawGyroRotation.position = transform.position;
         _rawGyroRotation.rotation = transform.rotation;
 
         // Wait until gyro is active, then calibrate to reset starting rotation.
-        yield return new WaitForSeconds(1);
+        // yield return new WaitForSeconds(1);
 
-        StartCoroutine(CalibrateAngles());
+        // StartCoroutine(CalibrateAngles());
     }
 
     private void Update()
     {
         ApplyGyroRotation();
-        ApplyCalibration();
+        Quaternion offsetRotation = Quaternion.Inverse(gyroInitialRotation) * _rawGyroRotation.rotation;
+        //ApplyCalibration();
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, _rawGyroRotation.rotation, _smoothing);
+        transform.rotation = Quaternion.Slerp(transform.rotation, initialRotation * offsetRotation, _smoothing);
     }
 
     private IEnumerator CalibrateAngles()
     {
         _tempSmoothing = _smoothing;
         _smoothing = 1;
-        _calibrationXAngle = _appliedGyroXAngle - _initialXAngle; // Offsets the y angle in case it wasn't 0 at edit time.
-        _calibrationYAngle = _appliedGyroYAngle - _initialYAngle; // Offsets the y angle in case it wasn't 0 at edit time.
-        _calibrationZAngle = _appliedGyroZAngle - _initialZAngle; // Offsets the y angle in case it wasn't 0 at edit time.
+        _calibrationXAngle = _appliedGyroXAngle - _initialAngles.x; // Offsets the y angle in case it wasn't 0 at edit time.
+        _calibrationYAngle = _appliedGyroYAngle - _initialAngles.y; // Offsets the y angle in case it wasn't 0 at edit time.
+        _calibrationZAngle = _appliedGyroZAngle - _initialAngles.z; // Offsets the y angle in case it wasn't 0 at edit time.
         yield return null;
         _smoothing = _tempSmoothing;
     }
@@ -58,9 +68,9 @@ public class GyroscopeControl : MonoBehaviour
     private void ApplyGyroRotation()
     {
         Quaternion tempGyroRotation = new Quaternion(
-            -Input.gyro.attitude.x, 
-            0f, 
-            -Input.gyro.attitude.y, 
+            Input.gyro.attitude.x, 
+            0.0f, 
+            Input.gyro.attitude.y, 
             Input.gyro.attitude.w);
         _rawGyroRotation.rotation = tempGyroRotation;
 
