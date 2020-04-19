7d5d3c30a3323f1953d559f7256fb822618f1ab1
Hugo <hugo.bollon@gmail.com>
2020-04-19 21:30:18 +0200

    Calibration of all axies on update

--- Assets/Scripts/GyroscopeControl.cs
+++ Assets/Scripts/GyroscopeControl.cs
@@ -8,8 +8,12 @@ public class GyroscopeControl : MonoBehaviour
     private float _initialXAngle = 0f;
     private float _initialYAngle = 0f;
     private float _initialZAngle = 0f;
+    private float _appliedGyroXAngle = 0f;
     private float _appliedGyroYAngle = 0f;
+    private float _appliedGyroZAngle = 0f;
+    private float _calibrationXAngle = 0f;
     private float _calibrationYAngle = 0f;
+    private float _calibrationZAngle = 0f;
     private Transform _rawGyroRotation;
     private float _tempSmoothing;
 
@@ -29,7 +33,7 @@ public class GyroscopeControl : MonoBehaviour
         // Wait until gyro is active, then calibrate to reset starting rotation.
         yield return new WaitForSeconds(1);
 
-        StartCoroutine(CalibrateYAngle());
+        StartCoroutine(CalibrateAngles());
     }
 
     private void Update()
@@ -40,11 +44,13 @@ public class GyroscopeControl : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, _rawGyroRotation.rotation, _smoothing);
     }
 
-    private IEnumerator CalibrateYAngle()
+    private IEnumerator CalibrateAngles()
     {
         _tempSmoothing = _smoothing;
         _smoothing = 1;
+        _calibrationXAngle = _appliedGyroXAngle - _initialXAngle; // Offsets the y angle in case it wasn't 0 at edit time.
         _calibrationYAngle = _appliedGyroYAngle - _initialYAngle; // Offsets the y angle in case it wasn't 0 at edit time.
+        _calibrationZAngle = _appliedGyroZAngle - _initialZAngle; // Offsets the y angle in case it wasn't 0 at edit time.
         yield return null;
         _smoothing = _tempSmoothing;
     }
@@ -57,16 +63,22 @@ public class GyroscopeControl : MonoBehaviour
             -Input.gyro.attitude.y, 
             Input.gyro.attitude.w);
         _rawGyroRotation.rotation = tempGyroRotation;
+
+        _appliedGyroXAngle = _rawGyroRotation.eulerAngles.x;
+        _appliedGyroYAngle = _rawGyroRotation.eulerAngles.y;
+        _appliedGyroZAngle = _rawGyroRotation.eulerAngles.z;
     }
 
     private void ApplyCalibration()
     {
+        _rawGyroRotation.Rotate(_calibrationXAngle, 0f, 0f, Space.World); // Rotates y angle back however much it deviated when calibrationYAngle was saved.
         _rawGyroRotation.Rotate(0f, -_calibrationYAngle, 0f, Space.World); // Rotates y angle back however much it deviated when calibrationYAngle was saved.
+        _rawGyroRotation.Rotate(0f, 0f, _calibrationZAngle, Space.World); // Rotates y angle back however much it deviated when calibrationYAngle was saved.
     }
 
     public void SetEnabled(bool value)
     {
         enabled = true;
-        StartCoroutine(CalibrateYAngle());
+        StartCoroutine(CalibrateAngles());
     }
 }
