01cdb9067604c330ee284c3211f89a7156be3769
Hugo <hugo.bollon@gmail.com>
2020-06-21 21:22:24 +0200

    New modulable sliding wall script through inspector + lvl 7-8

--- Assets/Scripts/GameObjects/SlidingWall.cs
+++ Assets/Scripts/GameObjects/SlidingWall.cs
@@ -1,5 +1,57 @@
 using UnityEngine;
+using System.Collections;
 
 public class SlidingWall : MonoBehaviour {
-    
+    public float speed = 1f;
+    public float distance = 1f;
+    public bool invertSliding = false;
+    public bool debug = false;
+
+    private void Start() {
+        StartCoroutine(Slide());
+    }
+
+    private IEnumerator Slide() {
+        Vector3 initialPosition = transform.localPosition;
+        for(;;){
+            if(!invertSliding){
+                transform.localPosition = new Vector3(transform.localPosition.x,
+                                                transform.localPosition.y, 
+                                                transform.localPosition.z + speed);
+
+                if(transform.localPosition.z >= (initialPosition.z + distance)){
+                    invertSliding = !invertSliding;
+                    transform.localPosition = new Vector3(transform.localPosition.x,
+                                                transform.localPosition.y, 
+                                                initialPosition.z + distance);
+                }
+            }
+            else{
+                transform.localPosition = new Vector3(transform.localPosition.x,
+                                                transform.localPosition.y, 
+                                                transform.localPosition.z - speed);
+                                                
+                if(transform.localPosition.z <= (initialPosition.z - distance)){
+                    invertSliding = !invertSliding;
+                    transform.localPosition = new Vector3(transform.localPosition.x,
+                                                transform.localPosition.y, 
+                                                initialPosition.z - distance);
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    private void OnGUI () {
+        if(debug){
+            GUIStyle style = new GUIStyle();
+            style.fontSize = Mathf.RoundToInt(Mathf.Min(Screen.width, Screen.height) / 20f);
+            style.normal.textColor = Color.white;
+            GUILayout.BeginVertical("box");
+            GUILayout.Label("Local position: " + transform.localPosition.ToString(), style);
+            GUILayout.EndVertical();
+        }
+    }
+
 }
