0a958ec4be95f8202f5524ed13012a7b6801e425
Hugo <hugo.bollon@gmail.com>
2020-06-27 00:15:38 +0200

    Add jump option on boost script + Night theme finished and polished
    polished some prefabs variant and material for night

--- Assets/Scripts/GameObjects/Boost.cs
+++ Assets/Scripts/GameObjects/Boost.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class Boost : MonoBehaviour {
 
     public float velocity = 10f;
+    public bool addJump = false;
 
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Ball")
             other.gameObject.GetComponent<Rigidbody>().AddForce(
-                new Vector3(velocity, 0f, 0f),
+                new Vector3(velocity, velocity * (addJump ? 1 : 0), 0f),
                 ForceMode.Impulse
             );
     }
