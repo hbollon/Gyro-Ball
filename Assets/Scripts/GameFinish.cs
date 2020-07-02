1ccb8f4d2e84089a2ab8df4b536850b310647ee8
Hugo <hugo.bollon@gmail.com>
2020-07-02 19:55:15 +0200

    Limit finishPoint ball to 1 & Add multiples balls support option

--- Assets/Scripts/GameFinish.cs
+++ Assets/Scripts/GameFinish.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 public class GameFinish : MonoBehaviour {
 
+    public bool multipleBallsSupport = false;
+    private int nbBallsIn = 0;
+
     private void OnCollisionEnter(Collision other) {
-        Ball ball = other.gameObject.GetComponent<Ball>();
-        if((other.gameObject.tag == "Ball") && (!ball.IsAtEnd()) && (!ball.IsAttached())){
-            ball.SetAtEnd(true);
-            other.gameObject.transform.parent = gameObject.transform;
-            ball.SetAttached(true);
-            ball.Finish();
-            LevelManager.Instance.NbBallsAtEnd++;
+        if(multipleBallsSupport || nbBallsIn < 1) {
+            Ball ball = other.gameObject.GetComponent<Ball>();
+            if((other.gameObject.tag == "Ball") && (!ball.IsAtEnd()) && (!ball.IsAttached())){
+                nbBallsIn++;
+                ball.SetAtEnd(true);
+                other.gameObject.transform.parent = gameObject.transform;
+                ball.SetAttached(true);
+                ball.Finish();
+                LevelManager.Instance.NbBallsAtEnd++;
+            }
         }
     }
 }
